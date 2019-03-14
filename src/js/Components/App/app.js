5f1676dce353ca82691d96a844f30d65682bcaf9
Dnzln <qvanoman@gmail.com>
2019-03-14 02:29:44 +0200

    Add Component, Temperature

--- src/js/Components/App/app.js
+++ src/js/Components/App/app.js
@@ -1,5 +1,17 @@
-export default class App {
+import Component from '../../framework/Component';
+import {Temperature} from '../Temterature';
+
+export default class App extends Component {
     constructor(host) {
-        host.innerHTML = 'ss sssdSs afasdsadsad';
+        super(host);
+    }
+    
+    render() {
+        const t1 = document.createElement('div');
+        new Temperature(t1);
+        const t2 = document.createElement('div');
+        new Temperature(t2);
+        
+        return ['Temperature range', t1, t2]
     }
 }
---
+++ src/js/Components/Temterature/index.js
@@ -0,0 +1,2 @@
+export {default as Temperature} from './Temperature';
+
